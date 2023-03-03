2af5094c7035c0bebbd9fa2ec898dc3d07a65d2c
Viacheslav009 <mstislavpushkov@gmail.com>
2023-03-03 13:17:15 +0200

    third commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+const imageContainer = document.querySelector('.gallery');
+const imageMarkup = creteImageCardMarkup(galleryItems);
+imageContainer.insertAdjacentHTML('beforeend', imageMarkup);
+imageContainer.addEventListener('click', imageClickAction);
+function creteImageCardMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+`;
+    })
+
+    .join('');
+}
+
+const options = {
+  // captions: true,
+  captionDelay: 250,
+
+  captionPosition: 'bottom',
+  captionsData: 'alt',
+};
+new SimpleLightbox('.gallery a', options);
+
+function imageClickAction(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
+}
