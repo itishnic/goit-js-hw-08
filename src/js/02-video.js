3c92b8d829c8d89bcb0f3f683c39fae0bab0a4dd
Viacheslav009 <mstislavpushkov@gmail.com>
2023-03-04 20:30:36 +0200

    part 2 work

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,46 @@
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+// player.on('timeupdate', function () {
+//   console.log('paused the video!');
+// });
+
+// const onPlay = function (data) {
+//   // data is an object containing properties specific to that event
+// };
+
+// player.on('play', onPlay);
+
+// player
+//   .getCurrentTime()
+//   .then(function (seconds) {
+//     // seconds = the current playback position
+//   })
+//   .catch(function (error) {
+//     // an error occurred
+//   });
+// player
+//   .setCurrentTime(3.456)
+//   .then(function (seconds) {
+//     // seconds = the actual time that the player seeked to
+//   })
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         // the time was less than 0 or greater than the video’s duration
+//         break;
+
+//       default:
+//         // some other error occurred
+//         break;
+//     }
+//   });
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
