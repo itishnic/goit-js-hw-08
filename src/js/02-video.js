38bbbc94f2df36c7410c435391ba8ab01ec81246
Viacheslav009 <mstislavpushkov@gmail.com>
2023-03-07 22:23:11 +0200

    update player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -45,5 +45,6 @@ player.on('timeupdate', throttle(onPlay, 1000));
 function onPlay({ seconds }) {
   localStorage.setItem(STORAGE_KEY, seconds);
 }
-
-player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+if (localStorage.getItem(STORAGE_KEY)) {
+  player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+}
