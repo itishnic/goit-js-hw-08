1d945d573f09a254911cf823e5b3a1ca1bad2171
Viacheslav009 <mstislavpushkov@gmail.com>
2023-03-08 19:37:19 +0200

    полный крах

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,17 +8,8 @@ const formData = {};
 dataFromLocalStorage();
 
 function onFormData(e) {
-  // formData[e.target.name] = e.target.value;
-  console.log(formData.email);
-  console.log(formData.message);
-  // if (formData.email === '' || formData.message === '') {
-  //   return alert('Please fill in all the fields!');
-  // }
-
   formData[e.target.name] = e.target.value;
-  if (formData.email === '' || formData.message === '') {
-    return alert('Please fill in all the fields!');
-  }
+
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
 
@@ -26,12 +17,18 @@ function dataFromLocalStorage() {
   const data = JSON.parse(localStorage.getItem('feedback-form-state'));
   const email = document.querySelector('.feedback-form input');
   const message = document.querySelector('.feedback-form textarea');
+  console.log(data);
   if (data) {
     email.value = data.email;
     message.value = data.message;
   }
 }
 function onSubmitForm(e) {
+  if (form.elements.email.value === '' || form.elements.message.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+
+  // console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
   console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
   e.preventDefault();
   e.currentTarget.reset();
